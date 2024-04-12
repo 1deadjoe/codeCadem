d66c26cbaeae30ce006924738f2b4053a460b736
1deadjoe <166827700+1deadjoe@users.noreply.github.com>
2024-04-12 16:23:33 +0300

    Add files via upload

---
+++ working_with_numbers_and_strings.py
@@ -0,0 +1,10 @@
+# my_string = 'camperbot'
+# camper = my_string[0:6]
+# cp = my_string[0:6:3] #[x:y:h] -->Where x is the starting index, y is the ending index, and h is the step (the amount of characters to skip over).
+# print(cp)
+
+my_string = 'camperbot'
+camperbot = my_string[::-1] #Reverses the whole string
+print(camperbot)
+
+#LUHN'S ALGORITHM
